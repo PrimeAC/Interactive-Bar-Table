6ef732283c3bb867c73597134b8be7b273588a76
Afonso Caetano <afonso.caetano@outlook.com>
2016-04-07 00:36:27 +0100

    add menu structure, menu and history footer fix

---
+++ Projeto/javascript.js
@@ -0,0 +1,9 @@
+function toggleFooter(id) { 
+  if(document.getElementById(id).style.height == "4.5%") {
+    document.getElementById(id).style.height = "12.5%";
+  }
+
+  else {
+    document.getElementById(id).style.height = "4.5%";
+  }
+}
